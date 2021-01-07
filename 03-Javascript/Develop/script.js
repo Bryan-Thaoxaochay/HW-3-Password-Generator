de4f0b6ebc53a5550d410324b1b4f9c521d37ca4
LittleMagician <bthaoxaochay@gmail.com>
2021-01-06 21:09:43 -0600

    created the generatepassword function

--- 03-Javascript/Develop/script.js
+++ 03-Javascript/Develop/script.js
@@ -17,25 +17,33 @@
 // Click the button, selection of criteria appear (prompts), criteria is chosen, password is created, password is shown (alert)
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); // This is selecting the generate ID from the HTML and making it a variable.
 
-// Password Criteria
- var length = prompt("Choose a number between 8 and 128 for your password length.")
-  var upperCase = alert("Can we include uppercase letters?")
-  var lowerCase = alert("Can we include lowercase letters?")
-  var numbers = alert("Can we include numbers?")
-  var specialCharacters = alert("Can we include special characters?")
+
 
 // Write password to the #password input
-function writePassword(length, upperCase, lowerCase, numbers, specialCharacters) {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() {
   
-  passwordText.value = password;
+
+  // Generate Password
+  function generatePassword(length, upperCase, lowerCase, numbers, specialCharacters) {
+    // Password Criteria
+    var length = prompt("Choose a number between 8 and 128 for your password length.")
+    var upperCase = alert("Can we include uppercase letters?")
+    var lowerCase = alert("Can we include lowercase letters?")
+    var numbers = alert("Can we include numbers?")
+    var specialCharacters = alert("Can we include special characters?")
+
+    
+  }
+  
+  var password = generatePassword();
+
+  var passwordText = document.querySelector("#password"); // This is selecting the password ID from the HTML and making it a variable.
+
+  passwordText.value = password; // This is calling the value of the password created. 
 
 }
 
-writePassword();
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // This is calling the writePassword function when the button is clicked.
